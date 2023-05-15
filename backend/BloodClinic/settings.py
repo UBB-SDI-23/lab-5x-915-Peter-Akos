a861d59c6c0981bab249180e1a629c1034a6e582
Péter Ákos <91620857+915-Peter-Akos@users.noreply.github.com>
2023-05-15 09:04:12 +0300

    Update settings.py

--- backend/BloodClinic/settings.py
+++ backend/BloodClinic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*pl-e$15zv#nx#_ra1jwfgfd(88vp$zi@%6c&u)$7ly#xwjpv@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '34.65.248.2']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '34.65.134.231']
 
 
 # Application definition

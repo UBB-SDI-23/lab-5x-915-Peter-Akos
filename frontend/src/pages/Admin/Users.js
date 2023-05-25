4049e337d8fae73d36dde550f2cb31b7bf8d1e5a
Péter Ákos <91620857+915-Peter-Akos@users.noreply.github.com>
2023-05-25 15:21:39 +0300

    Update Users.js

--- frontend/src/pages/Admin/Users.js
+++ frontend/src/pages/Admin/Users.js
@@ -33,11 +33,12 @@ const Users = () => {
     },
   ];
 
-  function handleDropDownChange(_, value) {
+  function handleDropDownChange(_, value, alma) {
     console.log(value.props.value)
     console.log(value)
     console.log(value._owner.key)
     console.log(data)
+    console.log(alma)
 
     for (let i = 0; i < 20; i++) {
         if (data[i].id === value._owner.key)
